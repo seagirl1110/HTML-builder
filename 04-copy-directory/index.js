cdacf3f39d52eba7ae1ae46437c5aac71dd8eb79
Marina Elizarova <seagirl1110@gmail.com>
2022-05-21 23:12:58 +0300

    feat: finish task 04-copy-directory/index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+    await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+
+    const filesCopy = await fs.promises.readdir(path.resolve(__dirname, 'files-copy'));
+    for (const file of filesCopy) {
+        await fs.promises.rm(path.resolve(__dirname, 'files-copy', file))
+    };
+
+    const files = await fs.promises.readdir(path.resolve(__dirname, 'files'));
+    for (const file of files) {
+        const filePath = path.resolve(__dirname, 'files', file);
+        const fileCopyPath = path.resolve(__dirname, 'files-copy', file);
+        await fs.promises.copyFile(filePath, fileCopyPath);
+    }
+}
+
+copyDir();
