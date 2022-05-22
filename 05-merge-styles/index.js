975fed3a092e8707edc607917b3851669635b932
Marina Elizarova <seagirl1110@gmail.com>
2022-05-22 11:57:11 +0300

    feat: finish task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'bundle.css'));
+
+fs.promises.readdir(path.resolve(__dirname, 'styles')).then((files) => {
+    files.forEach(file => {
+        const filePath = path.resolve(__dirname, 'styles', file);
+        fs.promises.stat(filePath).then((fileInfo) => {
+            if (!fileInfo.isFile()) { return }
+            const ext = file.split('.')[1];
+            if (ext !== 'css') { return }
+            const readableStream = fs.createReadStream(path.resolve(__dirname, 'styles', file), 'utf-8');
+            readableStream.on('data', data => output.write(data)); 
+        });
+    })
+});
